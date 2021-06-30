c144d031b51755a07be63dbbec87b0ed8eb29af2
EricXSH <70551931+EricXSH@users.noreply.github.com>
2021-06-29 23:10:41 -0400

    Add files via upload

---
+++ EfficientNetB1.py
@@ -0,0 +1,126 @@
+import numpy as np
+import keras.backend as K
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras.layers import Dense, Flatten, Dropout,BatchNormalization ,Activation
+from tensorflow.keras.models import Model, Sequential
+from keras.applications.nasnet import NASNetLarge
+from tensorflow.keras.callbacks import ReduceLROnPlateau, ModelCheckpoint, EarlyStopping
+from tensorflow.keras.optimizers import Adam, SGD
+import matplotlib.pyplot as plt
+
+
+ 
+
+train_dir = 'fer2013/train/'
+test_dir = 'fer2013/test/'
+
+row, col = 48, 48
+classes = 7
+
+train_datagen = ImageDataGenerator(rescale=1./255,
+                                   zoom_range=0.3,
+                                   horizontal_flip=True)
+
+training_set = train_datagen.flow_from_directory(train_dir,
+                                                batch_size=64,
+                                                target_size=(48,48),
+                                                shuffle=True,
+                                                color_mode='grayscale',
+                                                class_mode='categorical')
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+test_set = test_datagen.flow_from_directory(test_dir,
+                                                batch_size=64,
+                                                target_size=(48,48),
+                                                shuffle=True,
+                                                color_mode='grayscale',
+                                                class_mode='categorical')
+
+base_model = tf.keras.applications.EfficientNetB1(input_shape=(48,48,3),include_top=False,weights="imagenet")
+
+
+
+
+model=Sequential()
+model.add(base_model)
+model.add(Dropout(0.5))
+
+
+model.add(Flatten()) 
+model.add(Dense(512,activation = 'relu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.5))
+    
+
+model.add(Dense(7, activation='softmax'))
+
+model.compile(optimizer=Adam(lr=0.0001, decay=1e-6), 
+                  loss='categorical_crossentropy', 
+                  metrics=['accuracy'])
+
+import datetime
+from tensorflow.keras.callbacks import ModelCheckpoint, CSVLogger, TensorBoard, EarlyStopping, ReduceLROnPlateau
+
+chk_path = 'EfficientNetB1_V1.h5'
+log_dir = "checkpoint/logs/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+
+checkpoint = ModelCheckpoint(filepath=chk_path,
+                             save_best_only=True,
+                             verbose=1,
+                             mode='min',
+                             moniter='val_loss')
+
+earlystop = EarlyStopping(monitor='val_loss', 
+                          min_delta=0, 
+                          patience=3, 
+                          verbose=1, 
+                          restore_best_weights=True)
+                        
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', 
+                              factor=0.2, 
+                              patience=6, 
+                              verbose=1, 
+                              min_delta=0.0001)
+
+
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+csv_logger = CSVLogger('training_V1.log')
+
+callbacks = [checkpoint, reduce_lr, csv_logger]
+
+steps_per_epoch = training_set.n // training_set.batch_size
+validation_steps = test_set.n // test_set.batch_size
+
+hist = model.fit(x=training_set,
+                 validation_data=test_set,
+                 epochs=80,
+                 callbacks=callbacks,
+                 steps_per_epoch=steps_per_epoch,
+                 validation_steps=validation_steps)
+
+plt.rcParams['figure.figsize'] = [12, 5]
+fig, (ax1, ax2) = plt.subplots(1, 2)
+
+
+plt.figure(figsize=(14,5))
+plt.subplot(1,2,2)
+plt.plot(hist.history['accuracy'])
+plt.plot(hist.history['val_accuracy'])
+plt.title('Model Accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend(['train', 'test'], loc='upper left')
+
+plt.subplot(1,2,1)
+plt.plot(hist.history['loss'])
+plt.plot(hist.history['val_loss'])
+plt.title('model Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+
+plt.show()
